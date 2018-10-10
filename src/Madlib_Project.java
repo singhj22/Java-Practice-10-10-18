66e508ce4732cac17a22910eab01f55ceeafa46f
singhj22 <42975589+singhj22@users.noreply.github.com>
2018-10-10 13:14:46 -0400

    Add files via upload

---
+++ src/Madlib_Project.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+
+public class Madlib_Project 
+
+{
+
+	public static void main(String[] args) 
+	
+	{
+		Scanner keyboard = new Scanner(System.in) ;
+		
+		
+		System.out.print("Please enter a place: ") ;
+		String place= keyboard.nextLine();
+		
+		System.out.print("Please enter an adjective: ") ;
+		String adjective= keyboard.nextLine();
+		
+		System.out.print("Please enter a female celebrity: ") ;
+		String femaleCelebrity= keyboard.nextLine();
+		
+		System.out.print("Please enter a body part: ") ;
+		String bodyPart= keyboard.nextLine();
+		
+		System.out.print("Please enter a body part: ") ;
+		String bodyPart2= keyboard.nextLine();
+		
+		System.out.print("Please enter a human organ: ") ;
+		String humanOrgan= keyboard.nextLine();
+		
+		System.out.print("Please enter an adjective: ") ;
+		String adjective2= keyboard.nextLine();
+		
+		System.out.print("Please enter a male celebrity: ") ;
+		String maleCelebrity= keyboard.nextLine();
+		
+		System.out.print("Please enter an adjective: ") ;
+		String adjective3= keyboard.nextLine();
+		
+		System.out.print("Please enter a body part: ") ;
+		String bodyPart3= keyboard.nextLine();
+		
+		System.out.print("Please enter a body part: ") ;
+		String bodyPart4= keyboard.nextLine();
+		
+		System.out.print("Please enter a place: ") ;
+		String place2= keyboard.nextLine();
+		
+		System.out.print("Please enter the name of a celebrity: ") ;
+		String celebrity= keyboard.nextLine();
+		
+		System.out.print("Please enter an animal: ") ;
+		String animal= keyboard.nextLine();
+		
+		System.out.print("Please enter a verb: ") ;
+		String verb= keyboard.nextLine();
+		
+		System.out.print("Please enter a number: ") ;
+		String number= keyboard.nextLine();
+		
+		System.out.print("Please enter a body part: ") ;
+		String bodyPart5= keyboard.nextLine();
+		
+		System.out.print("Please enter an adjective: ") ;
+		String adjective4= keyboard.nextLine();
+		
+		System.out.print("Please enter an adjective: ") ;
+		String adjective5= keyboard.nextLine();
+		
+		System.out.print("Please enter a verb in past tense: ") ;
+		String verb2= keyboard.nextLine();
+		
+		
+		System.out.print("Once upon a time, in a place called " + place + ", there was\n" );
+		
+		System.out.print("a " + adjective + " princess named "+femaleCelebrity+". Her kingdom\n" );
+		
+		System.out.print("was huge, but her " + bodyPart + " was bigger. She was beautiful\n" );
+		
+		System.out.print("from her " + bodyPart2 + " to her " + humanOrgan+". One day she saw a\n");
+		
+		System.out.print(adjective2+" prince named "+maleCelebrity+". He had a\n");
+		
+		System.out.print(adjective3+" face. As soon as his "+bodyPart3+" touched her\n");
+		
+		System.out.print(bodyPart4+" they fell in love. They got married at ");
+		
+		System.out.print(place2+" the following day.\n");
+		
+		System.out.print("Not long after they had a baby. They decided to call him/her\n");
+		
+		System.out.print(celebrity+". He/She looked like a "+animal+". He/She\n");
+		
+		System.out.print("used to "+verb+" "+number+" times a day so that his/her\n");
+		
+		System.out.print(bodyPart5+" would be "+adjective4+" and "+adjective5+".\n");
+		
+		System.out.print("And they "+verb2+" happily ever after.");
+
+
+
+
+
+
+	}
+
+}
